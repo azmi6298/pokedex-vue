b1579aba716b7de2e21a2865fb5f1dc05152a2ba
azmi6298 <azmi.filkom@gmail.com>
2022-09-15 23:00:25 +0700

    update layout on index page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import CardComponent from "./components/CardComponent.vue";
 
 const isLoading = ref(true);
 const results = ref([]);
@@ -9,7 +10,7 @@ const baseURL = "https://pokeapi.co/api/v2";
 // fetch from pokedex api
 function fetchData() {
   if (nextUrl.value == "") {
-    fetch(`${baseURL}/pokemon/?limit=10`)
+    fetch(`${baseURL}/pokemon/?limit=12`)
       .then((res) => res.json())
       .then((data) => {
         nextUrl.value = data.next;
@@ -33,11 +34,15 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col px-36 py-16">
-    <div class="text-4xl">Pokedex App 👋🏼</div>
+  <div class="flex flex-col text-center px-36 py-16 mb-8">
+    <div class="text-4xl">Pokedex App ⚡️</div>
     <div v-if="!isLoading">
-      <pre>{{ results }}</pre>
-      <button class="btn btn-primary max-w-md" @click="fetchData">
+      <div class="grid grid-cols-4 gap-y-8">
+        <div v-for="(result, index) in results" :key="index">
+          <CardComponent :name="result.name" :url="result.url" />
+        </div>
+      </div>
+      <button class="btn btn-primary max-w-md mt-8" @click="fetchData">
         Load More
       </button>
     </div>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-body">
+      <span>{{ name }}</span>
+      <span>{{ url }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  url: String,
+});
+</script>
